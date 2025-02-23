05922161834547ffb932b873d025e1dbb6f818d1
milanshahi57 <milanshahi045@gmail.com>
2025-02-23 19:00:02 +0545

    cart page intialized

--- nepmart/urls.py
+++ nepmart/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', product_list, name='product_list'),
     path('products/<slug:slug>/', product_detail, name='product_detail'),
+        path('cart/', views.cart, name='cart'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- nepmart/views.py
+++ nepmart/views.py
@@ -1,18 +1,57 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html',)
 
 
-from django.shortcuts import render, get_object_or_404
-from .models import Product
-
+#product page
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product.html', {'products': products})
 
+#product detail page
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
     return render(request, 'product_detail.html', {'product': product})
+
+
+#cart page
+def cart(request):
+    cart = request.session.get('cart', {})
+    cart_items = []
+    total_price = 0
+
+    for product_id, quantity in cart.items():
+        product = Product.objects.get(id=product_id)
+        cart_items.append({
+            'product': product,
+            'quantity': quantity,
+            'subtotal': product.price * quantity
+        })
+        total_price += product.price * quantity
+
+    return render(request, 'cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', {})
+
+    if str(product_id) in cart:
+        cart[str(product_id)] += 1
+    else:
+        cart[str(product_id)] = 1
+
+    request.session['cart'] = cart
+    return redirect('cart')
+
+
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', {})
+
+    if str(product_id) in cart:
+        del cart[str(product_id)]
+
+    request.session['cart'] = cart
+    return redirect('cart')
